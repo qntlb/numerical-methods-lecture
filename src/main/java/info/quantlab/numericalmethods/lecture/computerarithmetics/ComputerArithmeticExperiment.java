c900ebaf36a08df7acadb20c4da6f712b0d07b4f
Christian Fries <email@christian-fries.de>
2020-04-26 14:24:57 +0200

    Added comments and output.

--- src/main/java/info/quantlab/numericalmethods/lecture/computerarithmetics/ComputerArithmeticExperiment.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/computerarithmetics/ComputerArithmeticExperiment.java
@@ -81,61 +81,66 @@ public class ComputerArithmeticExperiment {
 		System.out.println("2^(-53).... = " + Math.pow(2, -53));
 
 		System.out.println("_".repeat(79));
-		
-		
+
+
 		/*
 		 * Small experiments with special values like Infinity and NaN
 		 */
-		
+
 		System.out.println("Small experiments with special values like Infinity and NaN.\n");
-		
+
 		double zero = 0.0;
 		double oneOverZero = 1.0/zero;
-		
+
 		System.out.println("1/0         = " + oneOverZero);
-		
+
 		// Checking +0
-		
+
 		double plusZero = 1.0;
 		while(plusZero > 0) {
 			plusZero = plusZero / 2.0;
 		}
 		System.out.println("+0          = " + plusZero);
-		
+
 		double minusZero = -1.0;
 		while(minusZero < 0) {
 			minusZero = minusZero / 2.0;
 		}
 		System.out.println("-0          = " + minusZero);
-		
+
 		System.out.println("+0 == -0   is " + (plusZero == minusZero));
 		System.out.println("1/(+0)      = " + (1.0/plusZero));
 		System.out.println("1/(-0)      = " + (1.0/minusZero));
-		
+
 		double plusInfinity = 1.0/plusZero;
 		double minusInfinity = 1.0/minusZero;
-		
+
 		System.out.println("+infinity  +  -infinity = " + (plusInfinity+minusInfinity));
-		
+
 		double nan = Double.NaN;
 
 		System.out.println("NaN         = " + nan);
-		
+
 		System.out.println("sqrt(-1)    = " + Math.sqrt(-1.0));
-		
+
 		System.out.println("1+NaN       = " + (1+nan));
 
 		System.out.println("1+infinity  = " + (1 + plusInfinity));
-		
+
 		System.out.println("_".repeat(79));
 
-		
-		
+
+		/*
+		 * Experiments with Double.MAX_VALUE
+		 */
+
+		System.out.println("Small experiments with Double.MAX_VALUE (largest positive floating point number (before infinity).\n");
+
 		double maxDouble	= Double.MAX_VALUE;
 		double bigStep		= Math.pow(2, 1022-53);
 		double bigerStep	= Math.pow(2, 1023-53);
-	
-		
+
+
 		System.out.println("maxDouble            = " + maxDouble);
 		System.out.println("maxDouble+1000       = " + (maxDouble+1000));
 		System.out.println("maxDouble+bigStep    = " + (maxDouble+bigStep));
@@ -144,38 +149,48 @@ public class ComputerArithmeticExperiment {
 		System.out.println();
 		System.out.println("maxDouble+maxDouble-maxDouble   = " + (maxDouble+maxDouble-maxDouble));
 		System.out.println("maxDouble+(maxDouble-maxDouble) = " + (maxDouble+(maxDouble-maxDouble)));
-		
+
 		System.out.println("_".repeat(79));
-		
-		
+
+
+		/*
+		 * Experiment on loss of significance - solve a quadratic equation x^2 - 2*p*x + q = 0
+		 */
+
+		System.out.println("Experiment on loss of significance - solve a quadratic equation x^2 - 2*p*x + q = 0.\n");
+
 		double p = 10000000.0;
 		double q = 1.0;
-		
+
 		// solve x^2 -2px + q = 0
-		double x = p - Math.sqrt(p*p-q);
-		
+		double x = p - Math.sqrt(p*p-q);		// alternatively: try q / (p + Math.sqrt(p*p-q))
+
 		double error = x*x - 2*p*x + q;
-		
+
 		System.out.println("x       = " + x);
 		System.out.println("error   = " + error);
 		System.out.println("_".repeat(79));
-		
-		
-		
+
+
+		/*
+		 * Experiment on loss of significance - summation
+		 */
+
+		System.out.println("Experiment on loss of significance - summation.\n");
+
 		double value = 0.1;
 		int numberOfValues = 10;
-	
+
 		double sumOfValuesClassical = getSumOfValuesClassical(value, numberOfValues);
 		double averageClassical = sumOfValuesClassical / numberOfValues;
-			
+
 		System.out.println("Classical summation average = " + averageClassical);
-		
+
 		double sumOfValueKahan = getSumOfValuesKahan(value, numberOfValues);
 		double averageKahan = sumOfValueKahan / numberOfValues;
 		System.out.println("Kahan     summation average = " + averageKahan);
-		
 
-
+		System.out.println("_".repeat(79));
 	}
 
 	private static double getSumOfValuesKahan(double value, int numberOfValues) {
@@ -197,5 +212,4 @@ public class ComputerArithmeticExperiment {
 		}
 		return sum;
 	}
-
 }
