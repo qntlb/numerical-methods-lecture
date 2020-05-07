feb0a978b490c9ef6616ed0890b4b6379ebe1e16
Christian Fries <email@christian-fries.de>
2020-05-07 07:14:22 +0200

    Added plot used in the motivation of MC method.

---
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/RunningAverageOfIndicator.java
@@ -0,0 +1,61 @@
+package info.quantlab.numericalmethods.lecture.montecarlo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.DoubleUnaryOperator;
+
+import net.finmath.plots.Plot2D;
+
+public class RunningAverageOfIndicator {
+
+	public static void main(String[] args) throws Exception {
+
+		int numberOfPoints = 200;
+
+		List<Double> averageValues = getRunningAveragesOfIndicator(3 /* omega */, numberOfPoints, 1 /* seed */);
+		List<Double> averageValues2 = getRunningAveragesOfIndicator(3 /* omega */, numberOfPoints, 432 /* seed */);
+
+		DoubleUnaryOperator averageFunction		= x -> averageValues.get((int)x);
+		DoubleUnaryOperator averageFunction2	= x -> averageValues2.get((int)x);
+		DoubleUnaryOperator limitValueFunction	= x -> 1.0/6.0;
+
+		Plot2D plot = new Plot2D(0.0, numberOfPoints-1, numberOfPoints, new DoubleUnaryOperator[] {
+				averageFunction,
+				averageFunction2,
+				limitValueFunction
+		});
+
+		plot.setTitle("Plot of running average of indicator function (x[i] == \u03C9[j]))");
+		plot.setXAxisLabel("n");
+		plot.setYAxisLabel("S(n, \u03C9[j])");
+		plot.show();
+	}
+
+	private static List<Double> getRunningAveragesOfIndicator(int omega, int numberOfSamples, int seed) {
+
+		Random random = new Random(seed);
+
+		List<Double> averages = new ArrayList<>();
+
+		int sum = 0;
+		for(int i=0; i<numberOfSamples; i++) {
+
+			int drawing = random.nextInt(6)+1;					// integer in { 1,2,3,4,5,6 }
+
+			int indicator = (drawing == omega) ? 1 : 0;			// indicator function 
+
+			sum += indicator;									// sum up
+
+			double average = ((double)sum)/(i+1);				// calculation the average
+
+			averages.add(average);								// append the value to the list
+
+			System.out.println(i + "\t" + drawing + "\t" + indicator + "\t" + sum + "\t" + average);
+		}
+		System.out.println();
+
+		return averages;
+	}
+
+}
