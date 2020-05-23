f2f1d2ecbea2f8ebd6ee4f728fc23dfcd12b328c
Christian Fries <email@christian-fries.de>
2020-05-23 21:20:25 +0200

    Thread safe v.d.Corput sequence.

--- src/main/java/info/quantlab/numericalmethods/lecture/randomnumbers/VanDerCorputSequence.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/randomnumbers/VanDerCorputSequence.java
@@ -1,13 +1,36 @@
 package info.quantlab.numericalmethods.lecture.randomnumbers;
 
-public class VanDerCorputSequence {
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.DoubleSupplier;
 
+public class VanDerCorputSequence implements DoubleSupplier {
+
+	private final AtomicInteger index;
+	private final int base;
+	
 	public static void main(String[] args) {
 		for(int i=0; i<30; i++) {
 			double x = getVanDerCorputNumber(i, 2);
 			System.out.println(i + "\t" + x);
 		}
 	}
+	
+	public VanDerCorputSequence(int startIndex, int base) {
+		super();
+		this.index = new AtomicInteger(startIndex);
+		this.base = base;
+	}
+
+	
+	public VanDerCorputSequence(int base) {
+		this(0, base);
+	}
+
+	@Override
+	public double getAsDouble() {
+		return getVanDerCorputNumber(index.getAndIncrement(), base);
+	}
+
 	/**
 	 * @param index The index of the sequence starting with 0
 	 * @param base The base.
