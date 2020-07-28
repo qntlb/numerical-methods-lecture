53ddc3068dea0d59ae95bc04b3a500070516515a
Christian Fries <email@christian-fries.de>
2020-07-28 12:41:25 +0200

    Added code that checks the assignments.

---
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/check/MonteCarloBlackScholesModelFactoryImplementationChecker.java
@@ -0,0 +1,276 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. Contact: email@christian-fries.de.
+ *
+ * Created on 12.04.2020
+ */
+package info.quantlab.numericalmethods.lecture.montecarlo.check;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
+
+import info.quantlab.numericalmethods.lecture.montecarlo.MonteCarloBlackScholesModelFactory;
+import info.quantlab.reflection.ObjectConstructor;
+import net.finmath.exception.CalculationException;
+import net.finmath.functions.AnalyticFormulas;
+import net.finmath.montecarlo.assetderivativevaluation.AssetModelMonteCarloSimulationModel;
+import net.finmath.montecarlo.assetderivativevaluation.products.AbstractAssetMonteCarloProduct;
+import net.finmath.montecarlo.assetderivativevaluation.products.DigitalOption;
+import net.finmath.montecarlo.assetderivativevaluation.products.DigitalOptionDeltaLikelihood;
+import net.finmath.montecarlo.assetderivativevaluation.products.EuropeanOption;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.stochastic.Scalar;
+import net.finmath.time.TimeDiscretizationFromArray;
+
+public class MonteCarloBlackScholesModelFactoryImplementationChecker {
+
+	private static Function<RandomVariable, String> printAvgErr = x -> String.format("%10.7f \u00B1 %10.7f", x.getAverage(), x.getStandardError());
+
+	// Model properties
+	private static final double	initialValue   = 1.0;
+	private static final double	riskFreeRate   = 0.05;
+	private static final double	volatility     = 0.30;
+
+	// Process discretization properties
+	private static final int		numberOfPaths		= 200000;
+
+	private static final double initalTime = 0;
+	private static final int		numberOfTimeSteps	= 20;
+	private static final double		deltaT				= 0.5;
+
+	private static final int		seed				= 31415;
+
+	// Product properties
+	private static final double	maturity = 2.0;
+	private static final double	strike = 1.05;
+
+	private static double accuracy = 1E-11;
+	private static Random random = new Random(3141);
+
+	/**
+	 * Check if the class solves the exercise.
+	 * 
+	 * @param theClass The class to test;
+	 * @param whatToCheck A string, currently "basic" or "accuracy".
+	 * @return Boolean if the test is passed.
+	 */
+	public static boolean check(Class<?> theClass, String whatToCheck) {
+
+		switch(whatToCheck) {
+		case "strong":
+		default:
+		{
+			if(!checkBasicFunctionality(theClass)) {
+				System.out.println("\t Before we test the variance reduction, the valuation has to be correct.");
+				return false;
+			}
+			else {
+				boolean success = checkStrong(theClass);
+				if(success) {
+					System.out.println("\t Strong variance reduction test passed.");
+				}
+				else {
+					System.out.println("\t The variance reduction is not good enough for the strong variance reduction test.");
+				}
+				return success;
+			}
+		}
+		case "weak":
+			if(!checkBasicFunctionality(theClass)) {
+				System.out.println("\t Before we test the variance reduction, the valuation has to be correct.");
+				return false;
+			}
+			else {
+				boolean success = checkWeak(theClass);
+				if(success) {
+					System.out.println("\t Weak variance reduction test passed.");
+				}
+				else {
+					System.out.println("\t The variance reduction is not good enough.");
+				}
+				return success;
+			}
+		case "basic":
+			boolean success = checkBasicFunctionality(theClass);
+			if(success) {
+				System.out.println("\t Valuation test passed.");
+			}
+			else {
+				System.out.println("\t Your class does not work or the value of the asian option appears to be wrong.");
+			}
+			return success;
+		}
+	}
+
+	/**
+	 * Check basic functionality
+	 * 
+	 * @param theClass The class to test;
+	 * @return Boolean if the test is passed.
+	 */
+	public static boolean checkBasicFunctionality(Class<?> theClass) {
+		try {
+			/*
+			 * Create model using the factory "theClass"
+			 */
+			final AssetModelMonteCarloSimulationModel monteCarloBlackScholesModel = createModel(theClass);
+
+			/*
+			 * Create products
+			 */
+			AbstractAssetMonteCarloProduct europeanOption = new EuropeanOption(maturity, strike);
+			AbstractAssetMonteCarloProduct digitalOption = new DigitalOption(maturity, strike);
+
+			/*
+			 * Value a European call option
+			 */
+			double valueAnalytic = AnalyticFormulas.blackScholesOptionValue(initialValue, riskFreeRate, volatility, maturity, strike);
+			RandomVariable valueMonteCarlo = europeanOption.getValue(initalTime, monteCarloBlackScholesModel);
+
+			System.out.println("\nValuation (European Call Option):\n");
+			System.out.format("\t%20s: %10.3g\n", "analytic", valueAnalytic);
+			System.out.format("\t%20s: %s\n", "monte carlo", printAvgErr.apply(valueMonteCarlo));
+
+			/*
+			 * Value a digital option
+			 */
+			double valueDigitalAnalytic = AnalyticFormulas.blackScholesDigitalOptionValue(initialValue, riskFreeRate, volatility, maturity, strike);
+			RandomVariable valueDigitalMonteCarlo = digitalOption.getValue(initalTime, monteCarloBlackScholesModel);
+
+			System.out.println("\nValuation (European Digital Option):\n");
+			System.out.format("\t%20s: %10.3g\n", "analytic", valueDigitalAnalytic);
+			System.out.format("\t%20s: %s\n", "monte carlo", printAvgErr.apply(valueDigitalMonteCarlo));
+
+			return	(Math.abs(valueMonteCarlo.getAverage()- valueAnalytic) <= 0.002)
+					&&
+					(Math.abs(valueDigitalMonteCarlo.getAverage()- valueDigitalAnalytic) <= 0.002);
+		}
+		catch(Exception e) {
+			System.out.println("\nTest failed with exception:");
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Check accuracy of the implementation.
+	 * 
+	 * @param theClass The class to test;
+	 * @return Boolean if the test is passed.
+	 */
+	public static boolean checkWeak(Class<?> theClass) {
+		try {
+		/*
+		 * Create model using the factory "theClass"
+		 */
+		final AssetModelMonteCarloSimulationModel monteCarloBlackScholesModel = createModel(theClass);
+
+		/*
+		 * Create products
+		 */
+		AbstractAssetMonteCarloProduct europeanOption = new EuropeanOption(maturity, strike);
+		AbstractAssetMonteCarloProduct digitalOption = new DigitalOption(maturity, strike);
+
+		/*
+		 * Calculate delta of a European call option
+		 */
+		double delteEuropeanAnalytic = AnalyticFormulas.blackScholesOptionDelta(initialValue, riskFreeRate, volatility, maturity, strike);
+		RandomVariable deltaEuropeanMonteCarlo = getDeltaByFiniteDifference(monteCarloBlackScholesModel, europeanOption, 1E-5);
+
+		System.out.println("\nDelta (European Call Option):\n");
+		System.out.format("\t%20s: %10.3g\n", "analytic", delteEuropeanAnalytic);
+		System.out.format("\t%20s: %s\n", "monte carlo", printAvgErr.apply(deltaEuropeanMonteCarlo));
+
+		/*
+		 * Calculate delta of a digital option
+		 */
+		double delteDigitalAnalytic = AnalyticFormulas.blackScholesDigitalOptionDelta(initialValue, riskFreeRate, volatility, maturity, strike);
+		RandomVariable deltaDigitalMonteCarlo = getDeltaByFiniteDifference(monteCarloBlackScholesModel, digitalOption, 1E-5);
+
+		System.out.println("\nDelta (European Digital Option):\n");
+		System.out.format("\t%20s: %10.3g\n", "analytic", delteDigitalAnalytic);
+		System.out.format("\t%20s: %s\n", "monte carlo", printAvgErr.apply(deltaDigitalMonteCarlo));		
+
+		return	(Math.abs(deltaEuropeanMonteCarlo.getAverage()- delteEuropeanAnalytic) <= 0.002)
+				&&
+				(Math.abs(deltaDigitalMonteCarlo.getAverage()- delteDigitalAnalytic) <= 0.06);
+		}
+		catch(Exception e) {
+			System.out.println("\nTest failed with exception:");
+			e.printStackTrace();
+			return false;
+		}
+	}	
+
+	private static boolean checkStrong(Class<?> theClass) {
+		try {
+		/*
+		 * Create model using the factory "theClass"
+		 */
+		final AssetModelMonteCarloSimulationModel monteCarloBlackScholesModel = createModel(theClass);
+
+		/*
+		 * Create products
+		 */
+		AbstractAssetMonteCarloProduct europeanOption = new EuropeanOption(maturity, strike);
+		AbstractAssetMonteCarloProduct digitalOption = new DigitalOption(maturity, strike);
+
+		/*
+		 * Calculate delta of a European call option
+		 */
+		double delteEuropeanAnalytic = AnalyticFormulas.blackScholesOptionDelta(initialValue, riskFreeRate, volatility, maturity, strike);
+		RandomVariable deltaEuropeanMonteCarlo = getDeltaByFiniteDifference(monteCarloBlackScholesModel, europeanOption, 1E-5);
+
+		System.out.println("\nDelta (European Call Option):\n");
+		System.out.format("\t%20s: %10.3g\n", "analytic", delteEuropeanAnalytic);
+		System.out.format("\t%20s: %s\n", "monte carlo", printAvgErr.apply(deltaEuropeanMonteCarlo));
+
+		/*
+		 * Calculate delta of a digital option
+		 */
+		double delteDigitalAnalytic = AnalyticFormulas.blackScholesOptionDelta(initialValue, riskFreeRate, volatility, maturity, strike);
+		RandomVariable deltaDigitalMonteCarlo = getDeltaByFiniteDifference(monteCarloBlackScholesModel, digitalOption, 1E-5);
+		RandomVariable deltaDigitalLikelihood = new DigitalOptionDeltaLikelihood(maturity, strike).getValue(initalTime, monteCarloBlackScholesModel);
+
+		System.out.println("\nDelta (European Digital Option):\n");
+		System.out.format("\t%20s: %10.3g\n", "analytic", delteDigitalAnalytic);
+		System.out.format("\t%20s: %s\n", "monte carlo", printAvgErr.apply(deltaDigitalMonteCarlo));		
+		System.out.format("\t%20s: %s\n", "likelihood ratio", printAvgErr.apply(deltaDigitalLikelihood));		
+
+		return	(Math.abs(deltaDigitalMonteCarlo.getAverage()- delteDigitalAnalytic) <= 0.002)
+				&&
+				(Math.abs(deltaDigitalMonteCarlo.getStandardError()- deltaDigitalLikelihood.getAverage()) <= 0.01);
+		}
+		catch(Exception e) {
+			System.out.println("\nTest failed with exception:");
+			e.printStackTrace();
+			return false;
+		}
+
+	}	
+
+	private static RandomVariable getDeltaByFiniteDifference(AssetModelMonteCarloSimulationModel model, AbstractAssetMonteCarloProduct product, double shiftRelative) {
+		try {
+			double shift = initialValue*shiftRelative;
+
+			AssetModelMonteCarloSimulationModel modelUpShift = model.getCloneWithModifiedData(Map.of("initialValue", initialValue+shift));
+			RandomVariable valueUpShift = product.getValue(initalTime, modelUpShift);
+
+			AssetModelMonteCarloSimulationModel modelDnShift = model.getCloneWithModifiedData(Map.of("initialValue", initialValue-shift));
+			RandomVariable valueDnShift = product.getValue(initalTime, modelDnShift);
+
+			return valueUpShift.sub(valueDnShift).div(2*shiftRelative);
+		} catch (CalculationException e) {
+			return new Scalar(Double.NaN);
+		}
+	}
+
+	private static AssetModelMonteCarloSimulationModel createModel(Class<?> theClass) {
+
+		MonteCarloBlackScholesModelFactory modelFactory = ObjectConstructor.<MonteCarloBlackScholesModelFactory>create(theClass, MonteCarloBlackScholesModelFactory.class);
+
+		AssetModelMonteCarloSimulationModel monteCarloBlackScholesModel = modelFactory.getModel(initialValue, riskFreeRate, volatility, new TimeDiscretizationFromArray(initalTime, numberOfTimeSteps, deltaT), numberOfPaths);
+		return monteCarloBlackScholesModel;
+	}
+
+}
---
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/check/MonteCarloControlVariateExperiment.java
@@ -0,0 +1,73 @@
+package info.quantlab.numericalmethods.lecture.montecarlo.check;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.functions.AnalyticFormulas;
+import net.finmath.montecarlo.BrownianMotion;
+import net.finmath.montecarlo.BrownianMotionFromMersenneRandomNumbers;
+import net.finmath.montecarlo.assetderivativevaluation.MonteCarloAssetModel;
+import net.finmath.montecarlo.assetderivativevaluation.models.BlackScholesModel;
+import net.finmath.montecarlo.assetderivativevaluation.products.EuropeanOption;
+import net.finmath.montecarlo.model.ProcessModel;
+import net.finmath.montecarlo.process.EulerSchemeFromProcessModel;
+import net.finmath.montecarlo.process.MonteCarloProcess;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.time.TimeDiscretization;
+import net.finmath.time.TimeDiscretizationFromArray;
+
+public class MonteCarloControlVariateExperiment {
+
+	public static void main(String[] args) throws CalculationException {
+
+		double initialValue = 1.0;
+		double riskFreeRate = 0.05;
+		double volatility = 0.20;
+		
+		double initialTime = 0.0;
+		int numberOfTimeSteps = 1;
+		double deltaT = 5;
+
+		int numberOfPaths = 10000000;
+		int seed = 3141;
+		
+		double maturity = 5.0;
+		double strike1 = 1.0;
+		double strike2 = 1.6;
+		
+		// Model: Black Scholes
+		ProcessModel blackScholesModel = new BlackScholesModel(initialValue, riskFreeRate, volatility);
+
+		// Brownian Motion / Numerical Scheme
+		TimeDiscretization timeDiscretization = new TimeDiscretizationFromArray(initialTime, numberOfTimeSteps, deltaT);
+		BrownianMotion brownianMotion = new BrownianMotionFromMersenneRandomNumbers(timeDiscretization, 1, numberOfPaths, seed);
+		MonteCarloProcess process = new EulerSchemeFromProcessModel(blackScholesModel, brownianMotion);
+
+		// Monte-Carlo Valuation Model
+		MonteCarloAssetModel blackScholesMonteCarloModel = new MonteCarloAssetModel(process);
+		
+		// Analytic Value
+		double valueAnalytic = AnalyticFormulas.blackScholesOptionValue(initialValue, riskFreeRate, volatility, maturity, strike1);
+		
+		
+		RandomVariable underlying = blackScholesMonteCarloModel.getAssetValue(maturity, 0);
+		RandomVariable numeraireAtPayment = blackScholesMonteCarloModel.getNumeraire(maturity);
+		RandomVariable numeraireAtEval	= blackScholesMonteCarloModel.getNumeraire(0.0);
+		
+		RandomVariable payoffCall = underlying.sub(strike1).floor(0.0);
+		RandomVariable strangePayoff = underlying.sub(strike2).choose(payoffCall, payoffCall.squared().div(strike2-strike1));
+
+		RandomVariable valueExotic = strangePayoff.div(numeraireAtPayment).mult(numeraireAtEval);
+		RandomVariable valuePlain = payoffCall.div(numeraireAtPayment).mult(numeraireAtEval);
+		
+		double c = valuePlain.sub(valuePlain.average()).mult(valueExotic.sub(valueExotic.average())).getAverage() / valuePlain.getVariance();
+
+		RandomVariable valueControled = valueExotic.sub(valuePlain.sub(valueAnalytic).mult(c));
+		EuropeanOption option = new EuropeanOption(maturity, strike1);
+		double value = option.getValue(blackScholesMonteCarloModel);
+		
+		System.out.println("Plain call analytic valuation..........: " + valueAnalytic);
+		System.out.println("Plain call Monte-Carlo valuation (1)...: " + valuePlain.getAverage() + " \t\u00B1 " + valuePlain.getStandardError());
+		System.out.println("Plain call Monte-Carlo valuation (2)...: " + value);
+		System.out.println("Exotic product Monte-Carlo valuation...: " + valueExotic.getAverage() + " \t\u00B1 " + valueExotic.getStandardError());
+		System.out.println("Exotic product controled MC valuation..: " + valueControled.getAverage() + "\t\u00B1 " + valueControled.getStandardError());
+	}
+}
