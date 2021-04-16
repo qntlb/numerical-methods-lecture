cc210ec1991182903c03609739e53a3aed8abbca
Christian Fries <email@christian-fries.de>
2021-04-17 01:10:40 +0200

    Enlarged the interface slightly.

--- src/main/java/info/quantlab/numericalmethods/lecture/computerarithmetics/summation/DoubleVector.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/computerarithmetics/summation/DoubleVector.java
@@ -5,12 +5,30 @@
  */
 package info.quantlab.numericalmethods.lecture.computerarithmetics.summation;
 
+/**
+ * A class wrapping a vector of floating point doubles[] that allows to perform a summation.
+ * 
+ * @author Christian Fries
+ */
 public interface DoubleVector {
 
 	/**
-	 * Calculate the sum of the elements of the vector.
+	 * Calculates the sum of the elements of the vector, using error correction.
 	 *
 	 * @return The sum of the elements of the vector.
 	 */
 	double sum();
+
+	/**
+	 * Returns the number of elements of this vector.
+	 * 
+	 * @return The number of elements of this vector.
+	 */
+	long size();
+	
+	default double average() {
+		if(size() == 0) return Double.NaN;
+		
+		return sum() / size();
+	}
 }
