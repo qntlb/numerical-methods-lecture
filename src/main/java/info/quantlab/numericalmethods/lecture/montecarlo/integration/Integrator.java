8cca62d2f024cf5f21ce86a5085522ab1e61c88a
Christian Fries <email@christian-fries.de>
2021-05-09 11:44:00 +0200

    Added JavaDoc.

--- src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/integration/Integrator.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/integration/Integrator.java
@@ -1,7 +1,20 @@
 package info.quantlab.numericalmethods.lecture.montecarlo.integration;
 
+/**
+ * Interface to be implemented by integrators, i.e., classes that allow the integration
+ * of functions (Integrand) over a given domain (IntegrationDomain).
+ * 
+ * @author Christian Fries
+ */
 public interface Integrator {
 
+	/**
+	 * Calculate the integral \( \int_A f(x) dx \).
+	 * 
+	 * @param integrand The integrand f.
+	 * @param integrationDomain The integration domain A.
+	 * @return The integral \( \int_A f(x) dx \).
+	 */
 	double integrate(Integrand integrand, IntegrationDomain integrationDomain);
 
 }
--- src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/integration/MonteCarloIntegratorFactory.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/integration/MonteCarloIntegratorFactory.java
@@ -2,8 +2,20 @@ package info.quantlab.numericalmethods.lecture.montecarlo.integration;
 
 import info.quantlab.numericalmethods.lecture.randomnumbers.RandomNumberGenerator;
 
+/**
+ * Interface defining Integrator factories that require randomNumberGenrators (Monte-Carlo method).
+ * 
+ * @author Christian Fries
+ */
 public interface MonteCarloIntegratorFactory {
 
+	/**
+	 * Create a Monte-Carlo integrator using a specific RandomNumberGenerator and numberOfSamplePoints.
+	 * 
+	 * @param randomNumberGenerator The RandomNumberGenerator to be used.
+	 * @param numberOfSamplePoints The number of sample points to be used.
+	 * @return A class implementing the Integrator interface.
+	 */
 	Integrator getIntegrator(RandomNumberGenerator randomNumberGenerator, long numberOfSamplePoints);
 
 }
