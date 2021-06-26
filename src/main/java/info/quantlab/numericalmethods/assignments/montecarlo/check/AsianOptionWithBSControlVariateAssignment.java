ba7b61136ffe0bb4d5bc40f750867a4d01f421b0
Christian Fries <email@christian-fries.de>
2021-06-26 11:27:19 +0200

    Added interface for Control Variate Assigment

---
+++ src/main/java/info/quantlab/numericalmethods/assignments/montecarlo/check/AsianOptionWithBSControlVariateAssignment.java
@@ -0,0 +1,25 @@
+package info.quantlab.numericalmethods.assignments.montecarlo.check;
+
+import java.util.function.DoubleBinaryOperator;
+
+import info.quantlab.numericalmethods.lecture.montecarlo.integration.MonteCarloIntegratorFactory;
+import net.finmath.montecarlo.assetderivativevaluation.products.AssetMonteCarloProduct;
+import net.finmath.time.TimeDiscretization;
+
+public interface AsianOptionWithBSControlVariateAssignment {
+
+	/**
+	 * The solution of the exercise consists of the implementation of a class,
+	 * implementing a AssetMonteCarloProduct providing the valuation code
+	 * of an Asian option with a control variate for Black-Scholes models.
+	 * 
+	 * Construct a product representing an Asian option on an asset S (where S the asset with index 0 from the model - single asset case).
+	 * A(T) = 1/n sum_{i=1,...,n} S(t_i), where t_i are given observation times.
+	 *
+	 * @param strike The strike K in the option payoff max(A(T)-K,0).
+	 * @param maturity The maturity T in the option payoff maxAS(T)-K,0)
+	 * @param timesForAveraging The times t_i used in the calculation of A(T) = 1/n sum_{i=1,...,n} S(t_i).
+	 * @return A class implementing AssetMonteCarloProduct
+	 */
+	AssetMonteCarloProduct getAsianOption(Double maturity, Double strike, TimeDiscretization timesForAveraging);
+}
