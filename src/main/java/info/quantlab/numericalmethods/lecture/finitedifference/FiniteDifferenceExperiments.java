7a570d1bd0350d8d0e9abbc87ef6892a680ff81e
Christian Fries <email@christian-fries.de>
2021-07-07 15:33:25 +0200

    Added experiments for centered finite differences.

--- src/main/java/info/quantlab/numericalmethods/lecture/finitedifference/FiniteDifferenceExperiments.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/finitedifference/FiniteDifferenceExperiments.java
@@ -1,5 +1,7 @@
 package info.quantlab.numericalmethods.lecture.finitedifference;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.function.DoubleUnaryOperator;
 
@@ -28,23 +30,32 @@ public class FiniteDifferenceExperiments {
 		printForwardFiniteDifferenceApproximationOfExp(x, 1E-16 /* shift */);
 		printForwardFiniteDifferenceApproximationOfExp(x, 1E-15 /* shift */);
 		printForwardFiniteDifferenceApproximationOfExp(x, 1E-12 /* shift */);
+		printForwardFiniteDifferenceApproximationOfExp(x, 0.49 * Math.pow(2, -52) /* shift h */);
 		printForwardFiniteDifferenceApproximationOfExp(x, 0.51*Math.pow(2, -52) /* shift */);
 		printForwardFiniteDifferenceApproximationOfExp(x, 1.00*Math.pow(2, -52) /* shift */);
 		printForwardFiniteDifferenceApproximationOfExp(x, 1.50*Math.pow(2, -52) /* shift */);
 
-		plotForwardFiniteDifferenceApproximationErrorOfExp(x);
+		plotForwardFiniteDifferenceApproximationErrorOfExp(x, -16.5, -14);
+		plotForwardFiniteDifferenceApproximationErrorOfExp(x, -20, -1);
+		plotForwardFiniteDifferenceApproximationErrorOfExp(x, -10, -6);
+
+		plotCenteredFiniteDifferenceApproximationErrorOfExp(x, -16.5, -14);
+		plotCenteredFiniteDifferenceApproximationErrorOfExp(x, -20, -1);
+		plotCenteredFiniteDifferenceApproximationErrorOfExp(x, -7, -4);
+
 	}
 
 	private static void printForwardFiniteDifferenceApproximationOfExp(double x, double shift) {
 
 		double valueUpShift = Math.exp(x + shift);
 		double value = Math.exp(x);
+		
 		double finiteDifferenceApproximation = (valueUpShift-value)/shift;
 
 		System.out.println("h = " + String.format("%2.3e", shift) + "  \t  finite difference approx \u0394f / \u0394h = " + finiteDifferenceApproximation);
 	}
 
-	private static void plotForwardFiniteDifferenceApproximationErrorOfExp(double x) {
+	private static void plotForwardFiniteDifferenceApproximationErrorOfExp(double x, double scaleMin, double scaleMax) {
 
 		DoubleUnaryOperator finiteDifferenceApproxError = scale -> {
 
@@ -61,11 +72,39 @@ public class FiniteDifferenceExperiments {
 			return error;
 		};
 
-		Plot2D plot = new Plot2D(-16.5, -14 , 200, finiteDifferenceApproxError);
-		plot.setYAxisNumberFormat(new DecimalFormat("0.0"))
-		.setTitle("(One Sided Finite Difference) Derivative of exp(x) at x = " + x)
+		Plot2D plot = new Plot2D(scaleMin, scaleMax, 200, finiteDifferenceApproxError);
+		plot.setTitle("(One Sided Finite Difference) Derivative of exp(x) at x = " + x)
 		.setXAxisLabel("scale = log\u2081\u2080(h)  (h = 10^{scale})")
 		.setYAxisLabel("error")
+		.setYAxisNumberFormat(new DecimalFormat("0.0E00"))
+//		.saveAsPNG(new File("images/exp-x-forward-fd-("+(int)(scaleMin*10)+","+(int)(scaleMax*10)+").png"), 800, 500)
+		.show();
+	}
+
+	private static void plotCenteredFiniteDifferenceApproximationErrorOfExp(double x, double scaleMin, double scaleMax) {
+
+		DoubleUnaryOperator finiteDifferenceApproxError = scale -> {
+
+			double shift = Math.pow(10, scale);
+
+			double valueUpShift = Math.exp(x + shift);
+			double valueDnShift = Math.exp(x - shift);
+			double finiteDifferenceApproximation = (valueUpShift-valueDnShift)/shift/2.0;
+
+			double derivativeAnalytic = Math.exp(x);
+
+			double error = finiteDifferenceApproximation - derivativeAnalytic;
+
+			return error;
+		};
+
+		Plot2D plot = new Plot2D(scaleMin, scaleMax , 200, finiteDifferenceApproxError);
+		plot.setYAxisNumberFormat(new DecimalFormat("0.0"))
+		.setTitle("(Centered Finite Difference) Derivative of exp(x) at x = " + x)
+		.setXAxisLabel("scale = log\u2081\u2080(h)  (h = 10^{scale})")
+		.setYAxisLabel("error")
+		.setYAxisNumberFormat(new DecimalFormat("0.0E00"))
+//		.saveAsPNG(new File("images/exp-x-centered-fd-("+(int)(scaleMin*10)+","+(int)(scaleMax*10)+").png"), 800, 500)
 		.show();
 	}
 }
