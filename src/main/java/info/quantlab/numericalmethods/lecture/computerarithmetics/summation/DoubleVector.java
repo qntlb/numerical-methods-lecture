0d14079b995565eab1a33db7b7705f57304e0056
Christian Fries <email@christian-fries.de>
2022-05-02 12:24:25 +0200

    Added get(int i) to DoubleVector

--- src/main/java/info/quantlab/numericalmethods/lecture/computerarithmetics/summation/DoubleVector.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/computerarithmetics/summation/DoubleVector.java
@@ -1,7 +1,7 @@
 /*
  * (c) Copyright Christian P. Fries, Germany. Contact: email@christian-fries.de.
  *
- * Created on 12.04.2020
+ * Created on 12.04.2020, 01.05.2022
  */
 package info.quantlab.numericalmethods.lecture.computerarithmetics.summation;
 
@@ -11,6 +11,13 @@ package info.quantlab.numericalmethods.lecture.computerarithmetics.summation;
  * @author Christian Fries
  */
 public interface DoubleVector {
+
+	/**
+	 * Returns the i-th element of the vector (where i ranges from 0 (included) to size() (excluded).
+	 *
+	 * @return The i-th element of the vector.
+	 */
+	double get(int i);
 
 	/**
 	 * Calculates the sum of the elements of the vector, using error correction.
@@ -26,6 +33,12 @@ public interface DoubleVector {
 	 */
 	long size();
 
+	/**
+	 * Calculates the averages of the elements of the vector.
+	 * If size() is not zero, returns sum()/size(), otherwise Double.NAN.
+	 * 
+	 * @return If size() is not zero, returns sum()/size(), otherwise Double.NAN.
+	 */
 	default double average() {
 		if(size() == 0) return Double.NaN;
 
