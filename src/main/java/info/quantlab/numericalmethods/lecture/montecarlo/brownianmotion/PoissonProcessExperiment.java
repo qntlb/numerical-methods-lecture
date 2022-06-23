0a1812b05c194890fa889501c750e88abb339c70
Christian Fries <email@christian-fries.de>
2022-06-23 08:54:27 +0200

    Correct indentation.

--- src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/brownianmotion/PoissonProcessExperiment.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/brownianmotion/PoissonProcessExperiment.java
@@ -28,34 +28,40 @@ public class PoissonProcessExperiment {
 		double maturity = 10.0;
 		double lambda = 1.0;
 
+		/*
+		 * Part 1: Generate an array (numberOfPaths) containing the list of jump times < maturity.
+		 */
 		List<List<Double>> jumpTimesPaths = new ArrayList<List<Double>>();
 		for(int pathIndex = 0; pathIndex<numberOfPaths; pathIndex++) {
 			List<Double> jumpTimes = new ArrayList<>();
 
 			double nextJumpTime = 0;
 			while(nextJumpTime < maturity) {
-				
+
 				double uniform = Math.random();
 				double timeStep = - Math.log(uniform) / lambda;
-				
+
 				nextJumpTime += timeStep;
-				
+
 				if(nextJumpTime < maturity) jumpTimes.add(nextJumpTime);
 			}
 			jumpTimesPaths.add(jumpTimes);
 		}
 
+		/*
+		 * Part 2: Generate the function t -> N(t)
+		 */
 		DoubleToRandomVariableFunction process = time -> {
 			double[] values = new double[numberOfPaths];
-				for(int pathIndex = 0; pathIndex<numberOfPaths; pathIndex++) {
-					int count = 0;
-					for(Double jumpTime : jumpTimesPaths.get(pathIndex)) {
-						if(jumpTime > time) break;
-						count++;
-					}
-					values[pathIndex] = count - lambda * time;
+			for(int pathIndex = 0; pathIndex<numberOfPaths; pathIndex++) {
+				int count = 0;
+				for(Double jumpTime : jumpTimesPaths.get(pathIndex)) {
+					if(jumpTime > time) break;
+					count++;
 				}
-				return new RandomVariableFromDoubleArray(0.0, values);
+				values[pathIndex] = count - lambda * time;
+			}
+			return new RandomVariableFromDoubleArray(0.0, values);
 		};
 
 		int numberOfTimeSteps = 1000;
