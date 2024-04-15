f5b8ad41d664c146f4881ffa37f75c8c73168aff
Christian Fries <email@christian-fries.de>
2024-04-15 11:37:34 +0200

    Improved comments. Disabled saving the plots.

--- src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/bermudan/BermudanOptionExerciseInMonteCarloBlackScholesPlot.java
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/bermudan/BermudanOptionExerciseInMonteCarloBlackScholesPlot.java
@@ -49,23 +49,25 @@ public class BermudanOptionExerciseInMonteCarloBlackScholesPlot {
 		RandomVariable stockInT2 = model.getAssetValue(maturity2, 0);		// S(T2)
 		RandomVariable stockInT1 = model.getAssetValue(maturity1, 0);		// S(T1)
 
-		RandomVariable valueOption2InT2 = stockInT2.sub(strike2).floor(0.0);	// max(S(T2)-K,0)
-		RandomVariable valueOption1InT1 = stockInT1.sub(strike1).floor(0.0);	// max(S(T1)-K,0)
-		RandomVariable valueOption2InT1 = AnalyticFormulas.blackScholesOptionValue(stockInT1, riskFreeRate, volatility, maturity2-maturity1, strike2);
+		RandomVariable valueOption2InT2 = stockInT2.sub(strike2).floor(0.0);	// V2(T2) = max(S(T2)-K,0)
+		RandomVariable valueOption1InT1 = stockInT1.sub(strike1).floor(0.0);	// V1(T1) = max(S(T1)-K,0)
+		RandomVariable valueOption2InT1 = AnalyticFormulas.blackScholesOptionValue(stockInT1, riskFreeRate, volatility, maturity2-maturity1, strike2);	// V2(T1)
 
 		// Convert all time T-values to numeraire relative values by dividing by N(T)
 		RandomVariable valueRelativeOption2InT2 = valueOption2InT2.div(model.getNumeraire(maturity2));
 		RandomVariable valueRelativeOption1InT1 = valueOption1InT1.div(model.getNumeraire(maturity1));
 		RandomVariable valueRelativeOption2InT1 = valueOption2InT1.div(model.getNumeraire(maturity1));
 
+		// Exercise criteria: V2(T1) > V1(T1) ? V2(T2) ; V1(T1)
 		RandomVariable bermudanPathwiseValueAdmissible = valueRelativeOption2InT1.sub(valueRelativeOption1InT1)
 				.choose(valueRelativeOption2InT2, valueRelativeOption1InT1);
 
+		// Perfect foresight: V2(T2) > V1(T1) ? V2(T2) ; V1(T1)
 		RandomVariable bermudanPathwiseValueForesight = valueRelativeOption2InT2.sub(valueRelativeOption1InT1)
 				.choose(valueRelativeOption2InT2, valueRelativeOption1InT1);
 
-		var bermudanValue = bermudanPathwiseValueAdmissible.mult(model.getNumeraire(0.0)).getAverage();
-		var bermudanValueWithForsight = bermudanPathwiseValueForesight.mult(model.getNumeraire(0.0)).getAverage();
+		var bermudanValue				= bermudanPathwiseValueAdmissible.mult(model.getNumeraire(initialTime)).expectation().doubleValue();
+		var bermudanValueWithForsight	= bermudanPathwiseValueForesight.mult(model.getNumeraire(initialTime)).expectation().doubleValue();
 
 		System.out.println("Bermudan value with admissible exercise strategy...: " + bermudanValue);
 		System.out.println("Bermudan value with perfect forsight (wrong).......: " + bermudanValueWithForsight);
@@ -73,17 +75,16 @@ public class BermudanOptionExerciseInMonteCarloBlackScholesPlot {
 		/*
 		 * Plot the stuff
 		 */
-		Plotable2D continuationValues = PlotablePoints2D.of("Continuation Value max(S(T\u2082)-K\u2082, 0)", stockInT1, valueRelativeOption2InT2, new GraphStyle(new Rectangle(-1,-1,2,2), null, Color.RED));
-		Plotable2D exerciseValues = PlotablePoints2D.of("Underlying 1 (S(T\u2081)-K\u2081)", stockInT1, valueRelativeOption1InT1, new GraphStyle(new Rectangle(-3,-3,5,5), null, Color.GREEN));
-		Plotable2D expectedContinuationValue = PlotablePoints2D.of("Black Scholes Value of Option on S(T\u2082)-K\u2082", stockInT1, valueRelativeOption2InT1, new GraphStyle(new Rectangle(-3,-3,5,5),null, Color.BLUE));
-		Plotable2D bermudanPathwiseValueAdmissiblePlot = PlotablePoints2D.of("Bermudan exercise value", stockInT1, bermudanPathwiseValueAdmissible, new GraphStyle(new Rectangle(-1,-1,2,2), null, new Color(0.0f, 0.66f, 0.0f)));
-		Plotable2D bermudanPathwiseValueForesightPlot = PlotablePoints2D.of("Exercise value with foresight", stockInT1, bermudanPathwiseValueForesight, new GraphStyle(new Rectangle(-1,-1,2,2), null, new Color(0.50f, 0.50f, 0.0f)));
-		Plotable2D expectedContinuationValue2 = PlotablePoints2D.of("Black Scholes Value of Option on S(T\u2082)-K\u2082, conditional to T\u2081", stockInT1, valueRelativeOption2InT1, new GraphStyle(new Rectangle(-3,-3,5,5),null, Color.BLUE));
+		Plotable2D continuationValues			= PlotablePoints2D.of("Continuation Value max(S(T\u2082)-K\u2082, 0)",
+				stockInT1, valueRelativeOption2InT2, new GraphStyle(new Rectangle(-1,-1,2,2), null, Color.RED));
+
+		Plotable2D expectedContinuationValue	= PlotablePoints2D.of("Black Scholes Value of Option on S(T\u2082)-K\u2082, conditional to T\u2081",
+				stockInT1, valueRelativeOption2InT1, new GraphStyle(new Rectangle(-3,-3,5,5),null, Color.BLUE));
 
 		Plot plotCondExpEstimate = new Plot2D(
 				List.of(
 						continuationValues,
-						expectedContinuationValue2)
+						expectedContinuationValue)
 				)
 				.setYRange(-5, 150)
 				.setIsLegendVisible(true)
@@ -91,9 +92,14 @@ public class BermudanOptionExerciseInMonteCarloBlackScholesPlot {
 				.setYAxisLabel("V\u2082(T\u2081), V\u2082(T\u2082)")
 				.setTitle("Time T\u2081 and T\u2082 values related to a Bermudan option with exercises in T\u2081 and T\u2082.");
 		plotCondExpEstimate.show();
-		plotCondExpEstimate.saveAsSVG(new File("ConditionalExpectationEstimate.svg"),900,600);
-		plotCondExpEstimate.saveAsPDF(new File("ConditionalExpectationEstimate.png"),900,600);
+//		plotCondExpEstimate.saveAsJPG(new File("BermudanT1T2Values.jpg"),1200,675);
 
+		Plotable2D exerciseValues = PlotablePoints2D.of("Underlying 1 (S(T\u2081)-K\u2081)",
+				stockInT1, valueRelativeOption1InT1, new GraphStyle(new Rectangle(-3,-3,5,5), null, Color.GREEN));
+
+		Plotable2D bermudanPathwiseValueAdmissiblePlot = PlotablePoints2D.of("Bermudan pathwise value",
+				stockInT1, bermudanPathwiseValueAdmissible, new GraphStyle(new Rectangle(-1,-1,2,2), null, new Color(0.0f, 0.66f, 0.0f)));
+		
 		Plot plotBermudanExercise = new Plot2D(
 				List.of(
 						bermudanPathwiseValueAdmissiblePlot,
@@ -107,7 +113,10 @@ public class BermudanOptionExerciseInMonteCarloBlackScholesPlot {
 				.setYAxisLabel("V\u2081(T\u2081), V\u2082(T\u2081), V\u2082(T\u2082)")
 				.setTitle("Time T\u2081 and T\u2082 values related to a Bermudan option with exercises in T\u2081 and T\u2082.");
 		plotBermudanExercise.show();
-		plotBermudanExercise.saveAsSVG(new File("BermudanExerciseAdmissible.svg"),900,600);
+//		plotBermudanExercise.saveAsJPG(new File("BermudanT1T2ExericseValueAdmissible.jpg"),1200,675);
+
+		Plotable2D bermudanPathwiseValueForesightPlot = PlotablePoints2D.of("Bermudan pathwise value with foresight",
+				stockInT1, bermudanPathwiseValueForesight, new GraphStyle(new Rectangle(-1,-1,2,2), null, new Color(0.50f, 0.50f, 0.0f)));
 
 		Plot plotBermudanExerciseWithForesight = new Plot2D(
 				List.of(
@@ -122,6 +131,6 @@ public class BermudanOptionExerciseInMonteCarloBlackScholesPlot {
 				.setYAxisLabel("V\u2081(T\u2081), V\u2082(T\u2081), V\u2082(T\u2082)")
 				.setTitle("Time T\u2081 and T\u2082 values related to a Bermudan option with exercises in T\u2081 and T\u2082.");
 		plotBermudanExerciseWithForesight.show();
-		plotBermudanExerciseWithForesight.saveAsSVG(new File("BermudanExerciseForesight.svg"),900,600);
+//		plotBermudanExerciseWithForesight.saveAsJPG(new File("BermudanT1T2ExerciseValueWithForesight.jpg"),1200,675);
 	}
 }
