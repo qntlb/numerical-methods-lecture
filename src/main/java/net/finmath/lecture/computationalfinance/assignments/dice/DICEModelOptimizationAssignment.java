91b2963a58db37f7b023cd948eec434489ddc7bc
Christian Fries <email@christian-fries.de>
2025-02-10 22:06:38 +0100

    Added checker for new DICE assignment.

--- src/main/java/net/finmath/lecture/computationalfinance/assignments/dice/DICEModelOptimizationAssignment.java
+++ src/main/java/net/finmath/lecture/computationalfinance/assignments/dice/DICEModelOptimizationAssignment.java
@@ -1,18 +1,24 @@
 package net.finmath.lecture.computationalfinance.assignments.dice;
 
+import net.finmath.lecture.computationalfinance.assignments.dice.DICEModelOptimizationAssignment.AbatementParameterConstraint;
 import net.finmath.optimizer.SolverException;
 
 public interface DICEModelOptimizationAssignment {
+
+	public enum AbatementParameterConstraint {
+		NONE					/* none */,					
+		BOUNDED					/* mu is bounded between 0.0 and 1.0 */,
+		MONOTONE_AND_BOUNDED 	/* mu bounded and is monotone increasing */
+	}
 
 	/**
 	 * Returns the vector of optimal abatement factors &mu;<sub>i</sub>,
 	 * where &mu;<sub>i</sub> is the abatement factor for the time period
 	 * t<sub>i</sub> to t<sub>i+1</sub>.
 	 *
-	 * @param interestRate
+	 * @param abatementParameterConstraint A constraint on the abatement model.
+	 * @param interestRate The discount rate.
 	 * @return The vector of optimal abatement factors &mu;<sub>i</sub>.
-	 * @throws SolverException
 	 */
-	double[] getDICEModelOptimalAbatementPath(double interestRate) throws SolverException;
-
+	double[] getDICEModelOptimalAbatementPath(AbatementParameterConstraint abatementParameterConstraint, double interestRate);
 }
