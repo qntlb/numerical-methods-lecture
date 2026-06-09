63334959ba99a5e2d5a7e27f5c0fe6636b925e5d
Christian Fries <email@christian-fries.de>
2026-06-09 16:50:03 +0200

    Added class (missing in Integrator1DExperiment)

---
+++ src/main/java/info/quantlab/numericalmethods/lecture/montecarlo/integration1d/MonteCarloIntegrator1DFromRandomGenerator1D.java
@@ -0,0 +1,61 @@
+package info.quantlab.numericalmethods.lecture.montecarlo.integration1d;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Supplier;
+
+import info.quantlab.numericalmethods.lecture.randomnumbers.MersenneTwister;
+import info.quantlab.numericalmethods.lecture.randomnumbers.RandomNumberGenerator1D;
+
+/**
+ * Implementation of Integrator1D using Monte-Carlo integration with a given RandomNumberGenerator1D.
+ */
+public class MonteCarloIntegrator1DFromRandomGenerator1D implements Integrator1D {
+
+	private final int numberOfEvaluationPoints;
+	private final Supplier<RandomNumberGenerator1D> randomGenerator1DFactory;
+
+	/**
+	 * Create a Monte-Carlo integration with MersenneTwister (random number generator) and the given seed.
+	 * 
+	 * @param numberOfEvaluationPoints The number of sample points to be used.
+	 * @param randomGenerator1DFactory Random number generator factory.
+	 */
+	public MonteCarloIntegrator1DFromRandomGenerator1D(int numberOfEvaluationPoints, Supplier<RandomNumberGenerator1D> randomGenerator1DFactory) {
+		super();
+		this.numberOfEvaluationPoints = numberOfEvaluationPoints;
+		this.randomGenerator1DFactory = randomGenerator1DFactory;
+	}
+
+	/**
+	 * Create a Monte-Carlo integration with MersenneTwister (random number generator) and the given seed.
+	 * 
+	 * @param numberOfEvaluationPoints The number of sample points to be used.
+	 * @param seed The seed for the random number generator.
+	 */
+	public MonteCarloIntegrator1DFromRandomGenerator1D(int numberOfEvaluationPoints, int seed) {
+		super();
+		this.numberOfEvaluationPoints = numberOfEvaluationPoints;
+		this.randomGenerator1DFactory = () -> new MersenneTwister(seed);
+	}
+
+	@Override
+	public double integrate(DoubleUnaryOperator integrand, double lowerBound, double upperBound) {
+
+		// Create random number sequence with given generator
+		DoubleSupplier uniformRandomNumberGenerator = randomGenerator1DFactory.get();
+		double domainSize = upperBound-lowerBound;
+
+		double sum = 0.0;
+		for(int i=0; i<numberOfEvaluationPoints; i++) {
+
+			double randomNumber = uniformRandomNumberGenerator.getAsDouble();
+
+			double argument = lowerBound + randomNumber * domainSize;
+			double value = integrand.applyAsDouble(argument);
+
+			sum += value;
+		}
+		return sum / numberOfEvaluationPoints * domainSize;
+	}
+}
